933805ab021f49d904c2d1808178889ebb1b59ff
Mix Master Mike <mikeyamato@gmail.com>
2019-08-05 23:45:05 -0700

    completed section 4

--- README.md
+++ README.md
@@ -6,6 +6,108 @@ to run app, `node server.js`. Runs of port 4000.
 need a root query (an entry point of where to start the search)
 
 using 	`json-server` to mock an api call. `npm run json:server`. Runs on port 3000. 
-* http://localhost:3000/companies/1/users
+* `http://localhost:3000/companies/1/users` to see all users associated with company 1
 
 with Nodemon, `npm run dev`. 
+
+perfectly fine to query data that is already returned as nested data. already have company information but we're asking it again.
+```JavaScript
+{
+  company(id: "2") {
+    id
+    name
+    description
+    users{
+      firstName,
+      id
+      company{
+        name
+      }
+    }
+  }
+}
+```
+
+okay to name queries. much more beneficial for the FE. not so much when using GraphiQL. 
+```JavaScript
+query findCompany {
+  company(id: "2") {
+    id
+    name
+    description
+    users{
+      firstName,
+      id
+      company{
+        name
+      }
+    }
+  }
+}
+```
+
+okay to have multiple queries per request. just give repeating parent variables a different property name.
+```JavaScript
+{
+  google:company(id: "2") {
+    id
+    name
+    description
+  }
+  apple:company(id: "1") {
+    id
+    name
+    description
+  }
+}
+```
+
+query fragments help deal with duplication. 
+```JavaScript
+{
+  google:company(id: "2") {
+    ...companyDetails
+  }
+  apple:company(id: "1") {
+    ...companyDetails
+  }
+}
+
+fragment companyDetails on Company{
+  id
+  name
+  description
+}
+```
+
+# Mutations
+add or delete from the db
+do this by creating a completely new obj. don't update existing GraphQLObjectType
+
+must declare `mutation` like a query
+```JavaScript
+mutation {
+  addUser(
+    firstName:"Mike",
+    age:15
+  ){  // must always define the data we will get back
+    id
+    firstName
+    age
+  }
+}
+```
+the above comes back as...
+```JavaScript
+{
+  "data": {
+    "addUser": {
+      "id": "Qwwdfd3",
+      "firstName": "Mike",
+      "age": 15
+    }
+  }
+}
+```
+
+side note. `PUT` replaces the entire object. `PATCH` only replaces parts of an object. 
--- schema/schema.js
+++ schema/schema.js
@@ -5,17 +5,47 @@ const {
 	GraphQLObjectType,  // instruct gQL the presence of a user
 	GraphQLString,
 	GraphQLInt,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLNonNull
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	// to deal with circular reference since we are defining UserType after (Js limitation), wrap the fields value in an arrow function (anonymous function)
+	fields: () => ({
+		id: {type: GraphQLString},
+		name: {type: GraphQLString},
+		description: {type: GraphQLString},
+		users: {
+			type: new GraphQLList(UserType), // this is a one to many relationship (one company can have many employees). therefore `GraphQLList` needs to be used. 
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+				.then(resp => resp.data)
+			}
+		}
+	})
+})
 
 // create a new obj
 const UserType = new GraphQLObjectType({
 	name: 'User', // naming convention, uppercase
-	fields: {  // this tells gQL that every user will have an id, first name, and age. we need to define types. 
+	fields: () => ({  // this tells gQL that every user will have an id, first name, and age. we need to define types. 
 		id: {type: GraphQLString},
 		firstName: {type: GraphQLString},
-		age: {type: GraphQLInt}
-	}
+		age: {type: GraphQLInt}, 
+		// this will be used to associate company with user
+		company: {
+			type: CompanyType,
+			// use resolve function to associate a field from one table and associate it to another.
+			// think of resolve as taking us and connecting from one graph (aka node) to another 
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+				.then(resp => resp.data)  // axios returns data nested inside the data property
+				// console.log('parentValue: ', parentValue, 'args: ', args);
+			}
+		}
+	})
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -24,6 +54,7 @@ const RootQuery = new GraphQLObjectType({
 		user: {
 			type: UserType,
 			args: {
+				// whenever someone makes a query, it needs to be an id that is a string
 				id: {
 					type: GraphQLString
 				}
@@ -32,10 +63,80 @@ const RootQuery = new GraphQLObjectType({
 				return axios.get(`http://localhost:3000/users/${args.id}`)
 				.then(resp => resp.data)  // axios returns data nested inside the data property
 			}
+		},
+		// added in order to search a company directly from the root. otherwise to get to comany we'll always need to go through UserType
+		company: {
+			type: CompanyType,
+			args: {
+				id: {
+					type: GraphQLString
+				}
+			},
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+				.then(resp => resp.data)
+			}
+		}
+	}
+})
+
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		// best to name this to the action they perform
+		addUser: {
+			type: UserType,
+			// parameters to pass along when calling addUser
+			args: {
+				// wrap the types with GraphQLNonNull if they are a must have when passing parameters
+				firstName: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+				companyId: { type: GraphQLString }
+			},
+			// destructure parameters (optional)
+			resolve(parentValue, {firstName, age}){
+				return axios.post(`http://localhost:3000/users`, {
+					firstName,
+					age
+				})
+				.then(resp => resp.data)
+			}
+		},
+		deleteUser: {
+			type: UserType,
+			// parameters to pass along when calling addUser
+			args: {
+				// wrap the types with GraphQLNonNull if they are a must have when passing parameters
+				id: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			// destructure parameters (optional)
+			resolve(parentValue, {id}){
+				return axios.delete(`http://localhost:3000/users${id}`)
+				// null response is good
+				.then(resp => resp.data)
+			}
+		},
+		updateUser: {
+			type: UserType,
+			// parameters to pass along when calling addUser
+			args: {
+				// wrap the types with GraphQLNonNull if they are a must have when passing parameters
+				id: { type: new GraphQLNonNull(GraphQLString) },
+				firstName: { type: GraphQLString },
+				age: { type: GraphQLInt },
+				companyId: { type: GraphQLString }
+			},
+			// destructure parameters (optional)
+			resolve(parentValue, args){
+				return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+				// null response is good
+				.then(resp => resp.data)
+			}
 		}
 	}
 })
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation
 })
